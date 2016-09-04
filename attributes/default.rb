dd2b88262d9f3d6108ceaeda1e7d9d683eb84874
Taylor Monacelli <taylor.monacelli@streambox.com>
2016-09-04 15:16:19 -0700

    Add vc to php number comment

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,11 +4,13 @@ default['php']['package_name']               = 'PHP'
 default['php']['base_url']                   = "http://installer-bin.streambox.com"
 default['php']['version']                    = '5.6.10'
 
+# php 5.6.2 uses vc11 - 2012
 default['php']['5.6.10']['vcredist_ver']     = '11'
 default['php']['5.6.10']['vcredist_year']    = '12'
 default['php']['5.6.10']['x64']['checksum']  = 'd5dcabcdc61a6e2f81d36c3e4ce26543b3f80c235d3bbd57932aeb4387197546'
 default['php']['5.6.10']['x86']['checksum']  = '88cf0e5a05f7d5f1529c6ca27d569e7bdc0e85093c650e89f917206420e18045'
 
+# php 7.0.10 uses vc14 - 2015
 default['php']['7.0.10']['vcredist_ver']     = '14'
 default['php']['7.0.10']['vcredist_year']    = '15'
 default['php']['7.0.10']['x64']['checksum']  = '5d099320d05e7a52ee28c95be23ee1839327d70937346ccf485c41a080c6d33f'
