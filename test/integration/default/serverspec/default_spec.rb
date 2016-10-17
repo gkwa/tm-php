45259c61074c0761d456199ab4cc98c3a020f0d1
Taylor Monacelli <taylor.monacelli@streambox.com>
2016-10-16 17:09:08 -0700

    WIP: add first test

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,9 +1,31 @@
 require 'spec_helper'
 
 describe 'tm-php::default' do
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
-  end
+  case os[:family]
+  when 'windows'
+
+    cmd = "#{node['php']['install_dir']}\\php.exe"
+    v = "#{node['php']['version']}"
+
+    describe command('cmd /c "#{cmd}" --version') do
+      its(:stdout) { should match(/PHP #{v}/) }
+      its(:exit_status) { should eq 0 }
+    end
+
+  # when 'darwin'
+  #   describe command('/Applications/Firefox.app/Contents/MacOS/firefox -v') do
+  #     its(:stdout) { should match(/Mozilla Firefox /) }
+  #     its(:exit_status) { should eq 0 }
+  #   end
+  # when 'debian'
+  #   describe command('iceweasel -v') do
+  #     its(:stdout) { should match(/Mozilla Firefox /) }
+  #     its(:exit_status) { should eq 0 }
+  #   end
+  # else # linux
+  #   describe command('firefox -v') do
+  #     its(:stdout) { should match(/Mozilla Firefox /) }
+  #     its(:exit_status) { should eq 0 }
+  #   end
+  # end
 end
