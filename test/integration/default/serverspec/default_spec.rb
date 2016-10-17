b6a22210b27ddcb174ee7440c0929d23bd14d662
Taylor Monacelli <taylor.monacelli@streambox.com>
2016-10-16 18:28:33 -0700

    more trial error

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -19,21 +19,5 @@ describe 'tm-php::default' do
       its(:stdout) { should match(/PHP #{v}/) }
       its(:exit_status) { should eq 0 }
     end
-
-  # when 'darwin'
-  #   describe command('/Applications/Firefox.app/Contents/MacOS/firefox -v') do
-  #     its(:stdout) { should match(/Mozilla Firefox /) }
-  #     its(:exit_status) { should eq 0 }
-  #   end
-  # when 'debian'
-  #   describe command('iceweasel -v') do
-  #     its(:stdout) { should match(/Mozilla Firefox /) }
-  #     its(:exit_status) { should eq 0 }
-  #   end
-  # else # linux
-  #   describe command('firefox -v') do
-  #     its(:stdout) { should match(/Mozilla Firefox /) }
-  #     its(:exit_status) { should eq 0 }
-  #   end
-  # end
+  end
 end
